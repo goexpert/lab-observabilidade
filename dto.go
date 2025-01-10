9b41d1535be9f039d1740c5f6c881474ff8ce485
Rogerio Tadim <rogeriotadim@gmail.com>
2025-01-10 15:08:47 -0300

    merge files

--- dto.go
+++ dto.go
@@ -1,5 +1,15 @@
-package dto
+package lab
 
 type CepDto struct {
 	Cep string
+}
+
+type DtoError struct {
+	Message string `json:"msg"`
+}
+
+type LogradouroDto struct {
+	Cep        string `json:"cep"`
+	Localidade string `json:"localidade"`
+	Erro       string `json:"erro"`
 }
